5871a89a9f97b5a6c409712a544bac4fe771d221
JahleelT <jlt9287@nyu.edu>
2024-09-26 03:29:16 -0400

    added the info for the About me page on here

--- back-end/app.js
+++ back-end/app.js
@@ -3,6 +3,7 @@ const express = require('express') // CommonJS import style!
 const morgan = require('morgan') // middleware for nice logging of incoming HTTP requests
 const cors = require('cors') // middleware for enabling CORS (Cross-Origin Resource Sharing) requests.
 const mongoose = require('mongoose')
+const path = require('path')
 
 const app = express() // instantiate an Express object
 app.use(morgan('dev', { skip: (req, res) => process.env.NODE_ENV === 'test' })) // log all incoming requests, except when in unit test mode.  morgan has a few logging default styles - dev is a nice concise color-coded style
@@ -21,6 +22,9 @@ mongoose
 // load the dataabase models we want to deal with
 const { Message } = require('./models/Message')
 const { User } = require('./models/User')
+
+// Serve static files from the "public" directory
+app.use('/public', express.static(path.join(__dirname, 'public')))
 
 // a route to handle fetching all messages
 app.get('/messages', async (req, res) => {
@@ -76,6 +80,16 @@ app.post('/messages/save', async (req, res) => {
       status: 'failed to save the message to the database',
     })
   }
+})
+
+// a route to handle fetching the "About Us" content
+app.get('/about', (req, res) => {
+  const aboutContent = {
+    name: "Jahleel Townsend",
+    bio: "Hello, all! My name is Jahleel Townsend and I am a 21 year old university senior from Seattle, Washington. I study Computer Science under CAS, and I am looking to go into either cybersecurity or software engineering routes (maybe switching from one to the other in the future). I grew up in the greater seattle area for all of my life as a minor, first exploring outside of Washington when I decided to spend my first year of university abroad in NYU's Florence, Italy campus. \n\n    For personal interests, I enjoy walking, birdwatching, playing piano and guitar, trying new foods, listening to a wide range of music, and reading manga/manhwa/manhua. Something I would like to get into this semester is sketching. In particular, I want to sketch the birds that I take pictures of, though not in real time since they don't often sit still. \n\n    The reason I chose computer science as a field of interest is because I enjoy learning and working with languages, whether that be human or computer languages. Aside from the language component, being able to build working software from the ground up feels akin to building a complex puzzle, which I greatly enjoy; I used to build legos and do puzzles a lot as a child. Something that I would like to create one day is my own food-related app that works better for ranking food than Beli, better at finding food than Yelp, etc.",
+    imageUrl: "/public/images/IMG_7393_2.JPG"
+  }
+  res.json(aboutContent)
 })
 
 // export the express app we created to make it available to other modules
